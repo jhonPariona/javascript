2bfe3d5c5335b7442b3b2c36407488add0f75743
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-05-18 21:14:21 -0500

    🐛 NaN mejor lectura

--- NaN.js
+++ NaN.js
@@ -9,16 +9,15 @@ console.log(typeof NaN); /* number */
 var v = Number("n/a");
 console.log(v); /* NaN */
 
-console.log(
-  v === v
-); /* false por que la IEEE concensó que un NaN no es igual a otro NaN */
+/* false por que la IEEE concensó que un NaN no es igual a otro NaN */
+console.log(v === v);
 
-console.log(
-  isNaN("Hello")
-); /* true isNaN convierte primero a un numero y de ahi comprueba si es NaN */
+/* true isNaN convierte primero a un numero y de ahi comprueba si es NaN */
+console.log(isNaN("Hello"));
+
 console.log(isNaN(v)); /* true */
 
-console.log(
-  Number.isNaN("hello")
-); /* false ya que no hace la conversion antes de comprobar es6 */
+/* false ya que no hace la conversion antes de comprobar es6 */
+console.log(Number.isNaN("hello"));
+
 console.log(Number.isNaN(v)); /* true */
