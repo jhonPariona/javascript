94c2a19b017dd7ba7a813f2e3a2ca380b3cd3407
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-05-22 00:06:49 -0500

    📦 array indices numericos

--- array.js
+++ array.js
@@ -7,3 +7,22 @@ console.log("a: ", a); /* [stringKey: 'value'] */
 
 a[analiza] = "b";
 console.log("a: ", a); /* [stringKey: 'value', dinamicKey: 'b'] */
+
+/* -------------------------------------------------------------------------- */
+/*                                   length                                   */
+/* -------------------------------------------------------------------------- */
+// Se incrementa al agregar indices numericos y no con las propiedades
+
+var l = [];
+
+l.push("1");
+console.log(l); /* ['1'] */
+console.log(l.length); /* 1 */
+
+l.stringKey = "a";
+console.log(l); /* ['1', stringKey: 'a'] */
+console.log(l.length); /* 1 */
+
+l[10] = "10";
+console.log(l); /* ['1', , , , , , , , , , 10, stringKey: 'a'] */
+console.log(l.length); /* 11 */
